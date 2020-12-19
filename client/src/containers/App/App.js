a80a53849e1190bb93b95b6231b9faf0bc69324e
= <matan6080@gmail.com>
2020-12-19 15:09:48 +0200

    react-toastify package installed

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.scss";
+import { Redirect, Route, Switch, useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 export const App = () => {
   return <div>App</div>;
