757ed5c440bc6c23b1d8a25caf22b21e5b318fa2
= <matan6080@gmail.com>
2020-12-19 22:49:12 +0200

    Search options

--- server/src/controllers/book.ts
+++ server/src/controllers/book.ts
@@ -6,6 +6,7 @@ import {
   Get,
   Post,
   Delete,
+  Patch,
 } from "@overnightjs/core";
 import { Request, Response } from "express";
 import { body, param } from "express-validator";
@@ -21,6 +22,17 @@ export class BookController {
   @Get()
   private async getBooks(req: Request, res: Response) {
     const books = await Book.find();
+    res.status(200).send(books);
+  }
+
+  @Get(":bookSearch")
+  private async searchBooks(req: Request, res: Response) {
+    const { bookSearch } = req.params;
+    console.log(bookSearch);
+    const books = await Book.find({
+      publisher: { $regex: `.*${bookSearch}.*` },
+    });
+    console.log(books);
     res.status(200).send(books);
   }
 
@@ -50,5 +62,23 @@ export class BookController {
     console.log("book id", bookId);
     const doc = await Book.findOneAndRemove({ _id: bookId });
     res.send(doc ? "Book was deleted sucessufly" : {});
+  }
+
+  @Patch()
+  @Middleware([
+    body("id").isString().withMessage("Publisher name must be provided"),
+    body("book").isString().withMessage("Book name must be provided"),
+    body("author").isString().withMessage("Author must be provided"),
+    body("publisher").isString().withMessage("Publisher name must be provided"),
+    validateRequest,
+  ])
+  private async updateBook(req: Request, res: Response) {
+    const { id, book, author, publisher } = req.body;
+    const newBook = await Book.findOneAndUpdate(
+      { _id: id },
+      { $set: { book, author, publisher } },
+      { new: true }
+    );
+    res.status(202).send(newBook);
   }
 }
