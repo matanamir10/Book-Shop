5a070e04ae657e6bedf74b787d848689e024d889
= <matan6080@gmail.com>
2020-12-19 17:51:07 +0200

    BookList fetch and render books

--- client/src/components/BookList/BookList.js
+++ client/src/components/BookList/BookList.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./BookList.scss";
+import { Button } from "@material-ui/core";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchBooks } from "../../store/actions/book";
 import { Book } from "./Book/Book";
 
-const BookList = ({ books }) => {
+const BookList = () => {
+  const dispatch = useDispatch();
+  const { books } = useSelector((state) => state.book);
+
+  const getBooks = () => {
+    dispatch(fetchBooks());
+  };
   return (
     <ul className="book-list">
+      <Button>Show More</Button>
       {books && books.map((book) => <Book book={book} key={book.id} />)}
     </ul>
   );
