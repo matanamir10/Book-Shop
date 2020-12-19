389164b24bea1166f2add02d9b2d6c3aacb6b77f
= <matan6080@gmail.com>
2020-12-19 18:50:40 +0200

    Added action to book

--- client/src/components/BookList/Book/Book.js
+++ client/src/components/BookList/Book/Book.js
@@ -1,16 +1,48 @@
 import React from "react";
 import "./Book.scss";
-import { Card, CardContent, Typography } from "@material-ui/core";
+import {
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+} from "@material-ui/core";
+import { useSelector } from "react-redux";
 
 export const Book = ({ book }) => {
+  const { user } = useSelector((state) => state.auth);
+
+  let actions = (
+    <Button size="small" color="primary">
+      Purchase
+    </Button>
+  );
+  if (user.admin) {
+    actions = (
+      <>
+        <Button size="small" color="primary">
+          Update
+        </Button>
+        <Button size="small" color="primary">
+          Delete
+        </Button>
+      </>
+    );
+  }
   return (
     <li className="book">
       <Card className="book__card">
         <CardContent>
-          <Typography>{book.book}</Typography>
-          <Typography>{book.author}</Typography>
-          <Typography>{book.publisher}</Typography>
+          {Object.keys(book).map((bookKey) => {
+            return (
+              <Typography className="book__details" key={bookKey}>
+                <strong className="book__property">{bookKey}:</strong>
+                <p className="book__value">{book[bookKey]}</p>
+              </Typography>
+            );
+          })}
         </CardContent>
+        <CardActions>{actions}</CardActions>
       </Card>
     </li>
   );
