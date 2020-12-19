25d9b1376e607d430a3b6c7e40f73f0ec368deb9
= <matan6080@gmail.com>
2020-12-19 22:51:10 +0200

    Book search unctionallity

--- client/src/components/BookSearch/BookSearch.js
+++ client/src/components/BookSearch/BookSearch.js
@@ -1,10 +1,22 @@
 import React, { useState } from "react";
 import "./BookSearch.scss";
 import { Button } from "@material-ui/core";
+import { useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+import { searchBook } from "../../store/actions/book";
 import { Input } from "../../UI/Input";
 
 export const BookSearch = () => {
   const [search, setSearch] = useState("");
+  const dispatch = useDispatch();
+
+  const searchBooks = () => {
+    if (!search || search === "") {
+      toast.warning("Need to provide text");
+      return;
+    }
+    dispatch(searchBook(search));
+  };
   return (
     <div className="book-search">
       <Input
@@ -14,7 +26,12 @@ export const BookSearch = () => {
         onChange={(e) => setSearch(e.target.value)}
         placeholder="search book name"
       />
-      <Button className="book-search__cta" color="primary" variant="contained">
+      <Button
+        className="book-search__cta"
+        color="primary"
+        variant="contained"
+        onClick={searchBooks}
+      >
         Search Book
       </Button>
     </div>
