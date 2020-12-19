e03f712459c4d57e5fcde729ed3f0f0cec67449b
= <matan6080@gmail.com>
2020-12-19 18:50:17 +0200

    UI improve

--- client/src/components/BookSearch/BookSearch.js
+++ client/src/components/BookSearch/BookSearch.js
@@ -9,22 +9,20 @@ import {
 import { Visibility } from "@material-ui/icons";
 import { Input } from "../../UI/Input";
 
-const BookSearch = () => {
+export const BookSearch = () => {
   const [search, setSearch] = useState("");
   return (
     <div className="book-search">
-      <div>
-        <Input
-          id="book-search"
-          type="text"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-          placeholder="search"
-        />
-        <Button>Search Book</Button>
-      </div>
+      <Input
+        id="book-search"
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        placeholder="search books"
+      />
+      <Button className="book-search__cta" color="primary" variant="contained">
+        Search Book
+      </Button>
     </div>
   );
 };
-
-export default BookSearch;
