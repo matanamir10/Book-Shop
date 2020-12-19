0d26fdd3912c43aa59eef2ba4be5ce1bddccd16a
= <matan6080@gmail.com>
2020-12-19 15:17:52 +0200

    client setup for authenitcation

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.scss";
 import { Redirect, Route, Switch, useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import Auth from "../Auth/Auth";
+import { autoAuthenticate } from "../../store/actions/auth";
 
 export const App = () => {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const auth = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    dispatch(autoAuthenticate());
+  }, []);
+
+  useEffect(() => {
+    if (auth.isAuth) {
+      history.push("/book-store");
+    }
+  }, [auth]);
+
   return <div>App</div>;
 };
