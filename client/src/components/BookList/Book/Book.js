20dae588fca5ff8d4a5c90c3fc8029dab895acad
= <matan6080@gmail.com>
2020-12-19 18:23:41 +0200

    render card eith card

--- client/src/components/BookList/Book/Book.js
+++ client/src/components/BookList/Book/Book.js
@@ -1,6 +1,17 @@
 import React from "react";
 import "./Book.scss";
+import { Card, CardContent, Typography } from "@material-ui/core";
 
 export const Book = ({ book }) => {
-  return <li className="book">{book}</li>;
+  return (
+    <li className="book">
+      <Card className="book__card">
+        <CardContent>
+          <Typography>{book.book}</Typography>
+          <Typography>{book.author}</Typography>
+          <Typography>{book.publisher}</Typography>
+        </CardContent>
+      </Card>
+    </li>
+  );
 };
