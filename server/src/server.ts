91f494ca10925f0a20688060b23923c3690f9792
= <matan6080@gmail.com>
2020-12-19 15:44:07 +0200

    morgan package tp log http requests

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import morgan from "morgan";
 import { Server } from "@overnightjs/core";
 
 export class ServerApp extends Server {
   constructor() {
     super(true);
+    this.app.use(morgan("tiny"));
     this.app.use(bodyParser.json());
     this.app.use(cors({ origin: true, credentials: true }));
   }
