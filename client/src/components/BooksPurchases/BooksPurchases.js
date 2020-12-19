7ae744ad32e9d1eed44e211b8fd6196ed071f4c8
= <matan6080@gmail.com>
2020-12-19 18:50:01 +0200

    Book purchases component added

---
+++ client/src/components/BooksPurchases/BooksPurchases.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./BooksPurchases.scss";
+
+const BooksPurchases = () => {
+  return <div className="books-purchases">All books that i Have Purchsed</div>;
+};
+
+export default BooksPurchases;
