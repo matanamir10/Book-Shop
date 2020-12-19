c9b69b007053f407e153bf50e30089881f49759b
= <matan6080@gmail.com>
2020-12-19 15:54:18 +0200

    userModel added

---
+++ server/src/db/models/user.ts
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+interface UserAttrs {
+  email: string;
+  password: number;
+}
+
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+}
+
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+      maxlength: 20,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
+
+export { User };
